7407ca696d53458ab5d8d273286076462aeea3b6
hoya9802 <hoya9802@gmail.com>
2025-04-25 00:18:47 +0900

    modified lint

--- recipick_app/user/generate_token.py
+++ recipick_app/user/generate_token.py
@@ -7,6 +7,7 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 # credentials.json 파일 위치
 CREDENTIALS_FILE = 'recipick_app/user/secrets/credentials.json'
 
+
 def generate_token():
     """
     credentials.json을 사용하여 OAuth 인증을 진행하고 token.json 파일을 생성합니다.
@@ -15,13 +16,18 @@ def generate_token():
         CREDENTIALS_FILE, SCOPES
     )
     # access_type='offline'을 명시적으로 추가
-    creds = flow.run_local_server(port=8080, access_type='offline', prompt='consent')
+    creds = flow.run_local_server(
+        port=8080,
+        access_type='offline',
+        prompt='consent'
+    )
 
     # token.json 파일로 저장
     token_path = 'recipick_app/user/secrets/token.json'
     with open(token_path, 'w') as token:
         token.write(creds.to_json())
     print(f"token.json 파일이 {token_path}에 성공적으로 생성되었습니다!")
+
 
 if __name__ == '__main__':
     if not os.path.exists(CREDENTIALS_FILE):
