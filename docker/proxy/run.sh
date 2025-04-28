1b77a9adb7eacd9562d1e22214b2f90771087d98
hoya9802 <hoya9802@gmail.com>
2025-04-28 22:13:34 +0900

    fixed run.sh

--- docker/proxy/run.sh
+++ docker/proxy/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Checking for dhparams.pem"
-if [ ! -f "vol/proxy/ssl-dhparams.pem" ]; then
+if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
     echo "dhparams.pem does not exist - creating it"
     openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
 fi
