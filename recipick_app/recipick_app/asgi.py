294f3004779c2a4a4ca525c34b8f882cd4a5df21
hoya9802 <hoya9802@gmail.com>
2025-05-11 01:38:42 +0900

    update nginx

--- recipick_app/recipick_app/asgi.py
+++ recipick_app/recipick_app/asgi.py
@@ -1,25 +1,22 @@
-"""
-ASGI config for recipick_app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
 import os
 from django.core.asgi import get_asgi_application
+import django
+
+# 환경변수 설정 (Django 설정 파일을 지정)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipick_app.settings')
+
+# Django 초기화
+django.setup()
 
 # channels 라우팅과 미들웨어는 Django 초기화 이후에 가져와야 합니다.
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
-# 환경변수 설정
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipick_app.settings')
-
 # Django ASGI 애플리케이션 초기화
 django_asgi_app = get_asgi_application()
 
+# ASGI 프로토콜 타입별 라우팅 설정
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket":
